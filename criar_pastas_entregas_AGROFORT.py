bbc996f29e89b01f42d703241518d467b2bc11b8
luizzamprogno <lpzampronio@gmail.com>
2024-05-23 14:03:48 -0300

    comentarios adicionados

--- criar_pastas_entregas_AGROFORT.py
+++ criar_pastas_entregas_AGROFORT.py
@@ -1,7 +1,13 @@
 import os
 
+
+#Alterar o diretório base
 diretorio_base = 'C:/GeoCoder/Clientes/Nicholas/Teste'
+
+#Adicionar os diretórios a serem gerados em uma lista
 fazendas = ['Campo Alegre', 'Santa Leonor','São Miguel 1', 'São Miguel 2', 'Teste', 'Teste2']
+
+#Adicionar os subdiretórios a ser adicionados em cada diretório em uma lista
 suddiretorios = ['dwg', 'entregas', 'dados de campo', 'dados de projeto']
 
 
@@ -10,6 +16,7 @@ for diretorio in fazendas:
 	# Criar pastas das fazendas
 	diretorio_final = os.path.join(diretorio_base, diretorio)
 
+	# Verificar a existencia do diretório, e criá-lo
 	if not os.path.exists(diretorio_final):
 		os.mkdir(diretorio_final)
 	else:
@@ -25,15 +32,21 @@ for diretorio in fazendas:
 		else:
 			print(f'o Subdiretório {subdiretorio_final} já existe')
 
+
+		# Entrar somente na pasta 'entregas' de cada diretório criado
 		if subdiretorio_final.endswith('entregas'):
+			# Alterar o diretório para '/entregas'
 			os.chdir(subdiretorio_final)
 
+			# Dentro de 'entregas', criar a pasta 'dji'
 			subdiretorio_final_dji = os.path.join(subdiretorio_final, 'dji') 
 			if not os.path.exists(subdiretorio_final_dji):
 				os.mkdir(subdiretorio_final_dji)
 
+			# alterar o diretório para '/entregas/dji'
 			os.chdir(subdiretorio_final_dji)
 
+			# Dentro do diretório 'entregas/dji', criar a pasta 'shapefile'
 			subdiretorio_final_shapefile = os.path.join(subdiretorio_final, 'dji', 'shapefile')
 			if not os.path.exists(subdiretorio_final_shapefile):
 				os.mkdir(subdiretorio_final_shapefile)
